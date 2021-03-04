978bc2416a1c285708f1f630fadca48ea9c994c2
dhurba <dhurbag21@gmail.com>
2021-03-04 17:37:09 -0500

    add create suggestion feature

--- src/test/java/dev/ranieri/servicetests/SuggestionServiceTest.java
+++ src/test/java/dev/ranieri/servicetests/SuggestionServiceTest.java
@@ -1,6 +1,7 @@
 package dev.ranieri.servicetests;
 
 import dev.ranieri.daos.SuggestionDAO;
+import dev.ranieri.daos.SuggestionDaoLocal;
 import dev.ranieri.entities.Suggestion;
 import dev.ranieri.services.SuggestionService;
 import dev.ranieri.services.SuggestionServiceImpl;
@@ -25,8 +26,8 @@ public class SuggestionServiceTest {
 
     @BeforeEach
     void setUp(){
-        Suggestion suggestion1 = new Suggestion(0,"More breaks",10,0);
-        Suggestion suggestion2 = new Suggestion(0,"Coffe Maker is broken",7,0);
+        Suggestion suggestion1 = new Suggestion(0,"More breaks",100,0);
+        Suggestion suggestion2 = new Suggestion(0,"Coffe Maker is broken",-10,0);
         Suggestion suggestion3 = new Suggestion(0,"4 day work week",9,0);
 
         Set<Suggestion> suggestionSet = new HashSet<>();
@@ -34,6 +35,7 @@ public class SuggestionServiceTest {
         suggestionSet.add(suggestion2);
         suggestionSet.add(suggestion3);
         Mockito.when(suggestionDAO.getAllSuggestions()).thenReturn(suggestionSet);
+        Mockito.when(suggestionDAO.createSuggestion(suggestion1)).thenReturn(suggestion1);
         this.suggestionService = new SuggestionServiceImpl(this.suggestionDAO);
     }
 
@@ -54,4 +56,6 @@ public class SuggestionServiceTest {
         }
 
     }
+
+
 }
