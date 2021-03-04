127e09ca0062ead0b3bf7abc491d3af98c77d4d5
Adam Ranieri <adaman94@gmail.com>
2021-03-04 16:06:14 -0500

    added sorting feature

--- src/main/java/dev/ranieri/services/SuggestionServiceImpl.java
+++ src/main/java/dev/ranieri/services/SuggestionServiceImpl.java
@@ -3,7 +3,7 @@ package dev.ranieri.services;
 import dev.ranieri.daos.SuggestionDAO;
 import dev.ranieri.entities.Suggestion;
 
-import java.util.Set;
+import java.util.*;
 
 public class SuggestionServiceImpl implements SuggestionService{
 
@@ -20,7 +20,22 @@ public class SuggestionServiceImpl implements SuggestionService{
 
     @Override
     public Set<Suggestion> getAllSuggestions() {
-        return this.suggestionDAO.getAllSuggestions();
+        Set<Suggestion>  suggestions =this.suggestionDAO.getAllSuggestions();
+
+        Comparator<Suggestion> orderByPriority = (sugg1, sugg2) ->{
+
+            if(sugg1.getPriority()<sugg2.getPriority()){
+                return  1;
+            }
+            if(sugg1.getPriority()>sugg2.getPriority()){
+                return  -1;
+            }
+            return 0;
+        };
+
+        Set<Suggestion> orderedSuggestions = new TreeSet<>(orderByPriority);
+        orderedSuggestions.addAll(suggestions);
+        return orderedSuggestions;
     }
 
     @Override
